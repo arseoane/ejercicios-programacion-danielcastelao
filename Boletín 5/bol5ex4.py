efb087714e329280a3d5503aead5430173591f5c
Antón RS <arodriguezseoane@danielcastelao.org>
2025-09-24 18:02:44 +0200

    Update bol5ex4.py

--- Boletín 5/bol5ex4.py
+++ Boletín 5/bol5ex4.py
@@ -1,3 +1,5 @@
+# 4. Escribir un programa que imprima tódolos números pares entre dous números que se lle pidan o usuario.
+
 principio = int(input("Número inicial: "))
 final = int(input("Número final: "))
 
