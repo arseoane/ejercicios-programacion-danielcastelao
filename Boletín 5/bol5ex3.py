47a00e4cba99443e9c602ee9b665092c925779f7
Antón RS <arodriguezseoane@danielcastelao.org>
2025-09-24 18:02:18 +0200

    Update bol5ex3.py

--- Boletín 5/bol5ex3.py
+++ Boletín 5/bol5ex3.py
@@ -1,2 +1,4 @@
+# 3. Utiliza o programa anterior para xerar unha táboa de conversión de temperaturas, dende 0º F ata 120º F, de 10 en 10.
+
 for fahrenheit in range(0, 120, 10):
     print(str((fahrenheit - 32) * 5/9))
