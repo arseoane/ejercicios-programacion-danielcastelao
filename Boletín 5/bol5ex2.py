1918637c6572555ca3e5ff7882b4a56b60e98125
Antón RS <arodriguezseoane@danielcastelao.org>
2025-09-24 18:01:46 +0200

    Update bol5ex2.py

--- Boletín 5/bol5ex2.py
+++ Boletín 5/bol5ex2.py
@@ -1,3 +1,5 @@
+#2. Escribir un programa que converta un valor dado en grados Fahrenheit a grados Celsius. Recordade que a fórmula para a conversión é: F = 9/5 * C + 32.
+
 fahrenheit = int(input("Fahrenheit: "))
 celsius = (fahrenheit - 32) * 5/9
 print("Celsius: ", celsius)
