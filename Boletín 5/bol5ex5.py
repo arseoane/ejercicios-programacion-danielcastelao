d0ead948116817295bd5f8900618fa86630465ed
Antón RS <arodriguezseoane@danielcastelao.org>
2025-09-24 18:05:47 +0200

    Update bol5ex5.py

--- Boletín 5/bol5ex5.py
+++ Boletín 5/bol5ex5.py
@@ -1,4 +1,6 @@
-""" Escribir un programa que reciba un número n por parámetro e imprima os primeiros n números triangulares, xunto co seu índice. Os números triangulares obteñense mediante a suma dos números naturales dende 1 ata n. É dicir, si se piden os primeros 5 números triangulares, o programa debe imprimir:
+""" 5. Escribir un programa que reciba un número n por parámetro e imprima os primeiros n números triangulares, xunto co seu índice. 
+Os números triangulares obteñense mediante a suma dos números naturales dende 1 ata n. 
+É dicir, si se piden os primeros 5 números triangulares, o programa debe imprimir:
 1 - 1
 2 - 3
 3 - 6
