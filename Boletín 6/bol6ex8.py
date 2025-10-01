61e5c659cc1ce13fc8c16b1a19577aaceb7b86f7
Antón RS <arodriguezseoane@danielcastelao.org>
2025-10-01 14:13:41 +0200

    Update bol6ex8.py

--- Boletín 6/bol6ex8.py
+++ Boletín 6/bol6ex8.py
@@ -1,3 +1,6 @@
+""" 8. Escribir un programa que almacene nunha lista os seguintes prezos, 50, 75, 46, 22, 80, 65, 8, e mostre por pantalla o menor e o maior dos prezos.
+"""
+
 lista = [50, 75, 46, 22, 80, 65, 8]
 
 print(max(lista))
