bb6d3e7145b1ba385635d9925d9f2fd24da6437b
Antón RS <arodriguezseoane@danielcastelao.org>
2025-10-02 09:01:44 +0200

    Update bol6ex10.py

--- Boletín 6/bol6ex10.py
+++ Boletín 6/bol6ex10.py
@@ -1,4 +1,4 @@
-''' (Incompleto) 10. Escribir un programa que almacene as matrices
+''' 10. Escribir un programa que almacene as matrices
 
 a =	 (1,2,3)  				b = 	(-1,0)
    	 	(4,5,6)					(0,1)
@@ -8,10 +8,22 @@ Nota: Para representar matrices mediante listas usar listas anidadas, representa
 
 a = [(1,2,3),(4,5,6)]
 b = [(-1,0),(0,1),(1,1)]
-multi = 1
 
-for numa in a:
-    for numb in b:
-        multi *= numa * numb
+filas_a = len(a)
+cols_a = len(a[0])
+filas_b = len(b)
+cols_b = len(b[0])
 
-print(f"Produto: {multi}")
+if cols_a != filas_b:
+    print("Non se poden multiplicar.")
+else:
+    resultado = [[0 for _ in range(cols_b)] for _ in range(filas_a)]
+
+    for i in range(filas_a):
+        for j in range(cols_b):
+            for k in range(cols_a):
+                resultado[i][j] += a[i][k] * b[k][j]
+
+    print("Produto: ")
+    for fila in resultado:
+        print(fila)
