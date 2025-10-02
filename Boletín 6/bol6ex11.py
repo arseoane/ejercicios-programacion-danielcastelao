787312da3769d513add16bdac430e650b29faa61
Antón RS <arodriguezseoane@danielcastelao.org>
2025-10-02 09:39:19 +0200

    Create bol6ex11.py

---
+++ Boletín 6/bol6ex11.py
@@ -0,0 +1,15 @@
+import math
+'''11. Escribir un programa que pregunte por unha mostra de números, separados por comas, os garde nunha lista e mostre por pantalla a súa media e desviación típica.'''
+
+numeros = []
+
+pedir = int(input("Números a pedir: "))
+for i in range(pedir, 0, -1):
+    numeros.append(int(input("Número: ")))
+media = sum(numeros) / len(numeros)
+
+print(f"Media: {media}")
+varianza = sum((x - media) ** 2 for x in numeros) / len(numeros)
+desviacion = math.sqrt(varianza)
+
+print(f"Desviación estándar: {desviacion}")
