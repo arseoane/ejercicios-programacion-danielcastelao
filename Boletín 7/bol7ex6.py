39056aceecd21efbca7a7e344ff1990c61812b5e
arseoane <arodriguezseoane@danielcastelao.org>
2025-10-03 16:40:23 +0200

    Exercicio 6 rematado

--- Boletín 7/bol7ex6.py
+++ Boletín 7/bol7ex6.py
@@ -2,3 +2,11 @@
 e “paratodos. com”. Para posteriormente concaténalas e mostralas de novo.'''
 
 cadea = " www. pythonparatodos .com"
+
+parte1 = cadea[0:cadea.index("n")+1]
+parte2 = cadea[cadea.index("n")+1:]
+
+print("Parte1: ", parte1)
+print("Parte2: ", parte2)
+
+print("Cadea orixinal: ", cadea)
