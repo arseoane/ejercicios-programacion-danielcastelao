618986c6a9393260e3d13510c33640518af2709d
arseoane <arodriguezseoane@danielcastelao.org>
2025-10-03 10:55:12 +0200

    Exercicio 5 rematado

---
+++ Boletín 7/bol7ex5.py
@@ -0,0 +1,5 @@
+''' 5. Conta as vocais e as consoantes do String “Python Python Python”. Ollo cos espazos!'''
+
+cadea = "Python Python Python"
+cadea = cadea.replace(' ', '')
+print(f"Número de vogais e consoantes: {len(cadea)}")
