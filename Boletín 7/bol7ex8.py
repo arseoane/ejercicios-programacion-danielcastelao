68c8fa21815c6bc2810ec877e4017b0cf3774624
arseoane <arodriguezseoane@danielcastelao.org>
2025-10-03 16:46:42 +0200

    Exercicio 8 rematado

---
+++ Boletín 7/bol7ex8.py
@@ -0,0 +1,9 @@
+''' 8. Compara se o String “Python” é igual que o String “ JavaScript”.'''
+
+python = "Python"
+javascript = " JavaScript"
+
+if python == javascript:
+    print("Ambas cadeas son iguais.")
+else:
+    print("As cadeas non son iguais.")
