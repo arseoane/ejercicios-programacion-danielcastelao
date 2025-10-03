23d0950d6a547a692cba793ad9d70ea5b98d0069
arseoane <arodriguezseoane@danielcastelao.org>
2025-10-03 16:43:16 +0200

    Exercicio 7 rematado

---
+++ Boletín 7/bol7ex7.py
@@ -0,0 +1,8 @@
+''' 7. Transforma a cadea de texto “ Phytoneros” a maiúsculas. Garda o valor na variable e posteriormente
+convértea novamente a minúsculas.'''
+
+cadea = " Pythoneros"
+maiusculas = cadea.upper()
+print("Maiúsculas: ", maiusculas)
+minusculas = cadea.lower()
+print("Minúsculas: ", minusculas)
