d81104bd16d992932e2a759978d7570d97816722
arseoane <arodriguezseoane@danielcastelao.org>
2025-10-09 13:37:29 +0200

    Exercicio extra COMPLETADO

--- Extras/datavalida.py
+++ Extras/datavalida.py
@@ -5,6 +5,17 @@
 # / nas posicións correctas
 #evitar espazos inicio-fin
 
+def bisiesto(a):
+    if a % 4 == 0:
+        if a % 100 == 0:
+            if a % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
 
 valida = False
 dias31 = 1, 3, 5, 7, 8, 10, 12
@@ -15,23 +26,26 @@ data = input("[?] Pon unha data de formato dd/mm/aaaa (ex: 02/01/1998): ")
 if len(data) == 10:
     if "/" in data[2] and "/" in data[5]:
         dividir = data.split("/")
-        dia = int(dividir[0])
-        mes = int(dividir[1])
-        ano = int(dividir[2])
-        if mes > 12:
-            print("[!] Mes inválido!")
-        else:
-            if mes == 2:
-                if ano % 400 == 0 and dia <= 29:
-                    print("[+] Data válida!")
-                else:
-                    print("[!] Día inválido!")
-            elif mes in dias31 and dia > 31:
-                print("[!] Día inválido!")
-            elif mes in dias30 and dia > 30:
-                print("[!] Día inválido!")
+        if dividir[0].isdigit() and dividir[1].isdigit() and dividir[2].isdigit():
+            dia = int(dividir[0])
+            mes = int(dividir[1])
+            ano = int(dividir[2])
+            if mes > 12:
+                print("[!] Mes inválido!")
             else:
-                print("[+] Data válida!")
+                if mes == 2:
+                    if bisiesto(ano) == True and dia <= 29:
+                        print("[+] Data válida!")
+                    else:
+                        print("[!] Día inválido!")
+                elif mes in dias31 and dia > 31:
+                    print("[!] Día inválido!")
+                elif mes in dias30 and dia > 30:
+                    print("[!] Día inválido!")
+                else:
+                    print("[+] Data válida!")
+        else:
+            print("[!] Caracteres inválidos!")
 
     else:
         print("[!] Formato inválido!")
