503e6368fdc248761b59ab30a18bac487f0878fa
arseoane <arodriguezseoane@danielcastelao.org>
2025-10-16 16:58:38 +0200

    Exercicios engadidos.

--- Boletín 8/bol8ex1.py
+++ Boletín 8/bol8ex1.py
@@ -1,6 +1,8 @@
 '''1. Escribir unha función que reciba unha tupla de elementos e indique si se encontran ordeados de menor a maior ou non.'''
 
-tupla = (tuple(input('Elementos de tupla separados por ",": ').split(",")))
+lista = input('Elementos de tupla separados por ",": ').split(",")
+
+tupla = (tuple(lista))
 
 iguales = 0
 transtupla = []
@@ -10,11 +12,11 @@ for elemento in tupla:
 
 transtupla = transtupla.sort()
 
-for elemento, elemento2 in transtupla, tupla:
+for elemento, elemento2 in zip(lista, tupla):
     if elemento == elemento2:
         iguales += 1
 
-if iguales == len(tupla):
+if iguales == len(lista):
     print("A tupla está ordeada.")
 else:
     print("A tupla non está ordeada.")
