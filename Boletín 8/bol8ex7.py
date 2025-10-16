c18cf1a76e1f5f9ad527ded757a9e21495d5ac6e
arseoane <arodriguezseoane@danielcastelao.org>
2025-10-16 17:47:06 +0200

    Exercicios engadidos.

--- Boletín 8/bol8ex7.py
+++ Boletín 8/bol8ex7.py
@@ -1,4 +1,4 @@
-'''Dada unha lista de números enteiros e un enteiro k, escribir unha función que:
+'''7. Dada unha lista de números enteiros e un enteiro k, escribir unha función que:
     * Devolte tres listas, unha cos menores, outra cos maiores e outra cos iguais a k.
     * Devolte unha lista con aqueles que son múltiplos de k.
 '''
