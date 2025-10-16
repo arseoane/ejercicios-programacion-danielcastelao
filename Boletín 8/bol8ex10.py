ce663334e106e7314eaf497c20db0a2eb2663199
arseoane <arodriguezseoane@danielcastelao.org>
2025-10-16 17:41:44 +0200

    Exercicios engadidos.

---
+++ Boletín 8/bol8ex10.py
@@ -0,0 +1,13 @@
+'''10. Matrices.
+Escribir unha función que reciba duas matrices e devolte a suma.
+Escribir unha función que reciba duas matrices e devolte o produto.
+'''
+
+def sumar(matriz1, matriz2):
+    return sum(matriz1) + sum(matriz2)
+
+def produto(matriz1, matriz2):
+    return sum(matriz1) * sum(matriz2)
+
+print(sumar([1, 2, 3], [4, 5, 6]))
+print(produto([1, 2, 3], [4, 5, 6]))
