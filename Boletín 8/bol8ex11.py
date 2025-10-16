1590f8f83269732482c2ad37f20f6108fa5f4543
arseoane <arodriguezseoane@danielcastelao.org>
2025-10-16 17:46:58 +0200

    Exercicios engadidos.

---
+++ Boletín 8/bol8ex11.py
@@ -0,0 +1,15 @@
+'''11. Pregado de un texto. Escribir unha función que reciba un texto e unha lonxitude e devolva unha lista de cadeas de como máximo esa
+lonxitude. As líñas deben cortarse correctamente nos espazos (sen cortar as palabras).
+'''
+
+def listacar(texto, lon):
+    lista = []
+    i = 0
+    for car in texto:
+        lista.append(car)
+        i += 1
+        if i >= lon:
+            break
+    return lista
+
+print(listacar("Hola buenas señores", 5))
