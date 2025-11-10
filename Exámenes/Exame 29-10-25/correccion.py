c97b11ed180b9f95a9400eb5de8bb80d203e7a17
arseoane <arodriguezseoane@danielcastelao.org>
2025-11-10 09:51:27 +0100

    Exame corrixido

--- Exámenes/Exame 29-10-25/correccion.py
+++ Exámenes/Exame 29-10-25/correccion.py
@@ -12,3 +12,24 @@ def calcularMediaTemperaturas(temperaturas):
         totalTemperatura += temperatura
     return totalTemperatura/len(temperaturas)
 
+def diasSuperanMediaTemperatura(temperaturas):
+    dias = 0
+    mediaTemperatura = calcularMediaTemperaturas(temperaturas)
+    for temperatura in temperaturas:
+        if temperatura > mediaTemperatura:
+            dias += 1
+    return dias
+
+def mostraDiasSuperanTemperatura(temperaturas,temperaturaLimite):
+    dias = ["luns","martes","mércores","xoves","venres","sábado","domingo"]
+    for i, temperatura in enumerate(temperaturas):
+        if temperatura > temperaturaLimite:
+            print("0 dia"+ dias[i] + "superou o valor " + temperaturaLimite + " en " + temperatura + " graos.")
+
+temperaturas = almacenarTemperaturas()
+media = calcularMediaTemperaturas(temperaturas)
+print("A media é " + media + ".")
+numeroDias = diasSuperanMediaTemperatura(temperaturas)
+print(str(numeroDias) + " dias que superan media de temperaturas. ")
+print("Días que a temperatura supera a media: ")
+mostraDiasSuperanTemperatura(temperaturas,media)
