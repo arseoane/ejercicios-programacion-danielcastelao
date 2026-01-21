b7a99433028c3623eb54e560243251232b4ed014
arseoane <arodriguezseoane@danielcastelao.org>
2026-01-21 18:45:03 +0100

    Bol 11 ex 3

---
+++ Boletín 11/exercicio4.py
@@ -0,0 +1,29 @@
+# Crea a aplicación que permita gardar e recuperar os datos dos clientes dunha empresa. Para o cal, defina a clase Cliente, que teña os atributos:
+# id, identificador de cliente
+# nome
+# teléfono
+
+class Cliente():
+    idcount = 0
+    def __init__(self, nome, telefono):
+        self.id = Cliente.idcount
+        self.nome = nome
+        self.telefono = telefono
+
+        Cliente.idcount += 1
+
+    def __str__(self):
+        return f"{self.id},{self.nome},{self.telefono}\n"
+
+print('''
+1. Gardar cliente.
+
+2. Listar cliente.
+''')
+
+opcion = input("Ingrese una opción: ")
+
+if opcion == "1":
+    cliente = Cliente(input("Nome: "),input("Teléfono: "))
+    with open("clientes.txt","a") as f:
+        f.write(str(cliente))
