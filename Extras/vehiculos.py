04848d2802a73d08ed5224a4e9181aa6d5ca862c
arseoane <arodriguezseoane@danielcastelao.org>
2026-01-30 11:04:06 +0100

    Datas (clases)

--- Extras/vehiculos.py
+++ Extras/vehiculos.py
@@ -37,8 +37,8 @@ class Vehiculo(object):
     def mostrarDatos(self):
         return f"{self.matricula}, {self.velocidadeMaxima}, {self.autonomia}"
 
-class Punto():
-    def __init__(self,x,y):
+class Punto:
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
@@ -46,7 +46,38 @@ class Punto():
         return f"Punto: X: {self.x}, Y: {self.y}"
 
     def calcularArea(self):
-        return (self.x * self.x) + (self.y * self.y)
+        return self.x * self.x + self.y * self.y
 
     def calcularVolume(self):
-        return (self.x * self.x) + (self.y * self.y)
+        return self.x * self.x + self.y * self.y
+
+class Circulo(Punto):
+    def __init__(self, x, y, radio):
+        super().__init__(x, y)
+        self.radio = radio
+
+    def __str__(self):
+        return f"Circulo: Radio: {self.radio}, X: {self.x}, Y: {self.y}"
+
+    def calcularArea(self):
+        import math
+        return math.pi * self.radio * self.radio
+
+    def calcularVolume(self):
+        return 0
+
+class Cilindro(Circulo):
+    def __init__(self, x, y, radio, altura):
+        super().__init__(x, y, radio)
+        self.altura = altura
+
+    def __str__(self):
+        return f"Cilindro: Radio: {self.radio}, Altura: {self.altura}, X: {self.x}, Y: {self.y}"
+
+    def calcularArea(self):
+        import math
+        return 2 * math.pi * self.radio * (self.radio + self.altura)
+
+    def calcularVolume(self):
+        import math
+        return math.pi * self.radio * self.radio * self.altura
