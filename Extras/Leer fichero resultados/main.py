007194ffb3ad72c0801dbf148c5b83dfec25af7a
arseoane <arodriguezseoane@danielcastelao.org>
2026-02-20 11:31:51 +0100

    Extra

--- Extras/Leer fichero resultados/main.py
+++ Extras/Leer fichero resultados/main.py
@@ -2,5 +2,16 @@ with open("fichero.txt","r") as fichero:
     contenido = fichero.read()
     i = 0
     for linea in contenido.split("\n"):
-        print(f"{i+1}. {linea}")
+        print(f"Línea {i+1}: {linea}")
         i += 1
+
+def importacion_resultado(self, filePath):
+    try:
+        with open(filePath,"r") as f:
+            for i in f.readlines():
+                self.__xogadores.append(i)
+    except FileNotFoundError as e:
+        print(f"File not found, errror: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+
