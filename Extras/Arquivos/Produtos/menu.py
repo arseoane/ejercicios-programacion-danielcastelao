032af862d8f2dc230ee9c6ce618843e6f153021c
arseoane <arodriguezseoane@danielcastelao.org>
2026-02-27 10:57:26 +0100

    Creación de creación de produtos

--- Extras/Arquivos/Produtos/menu.py
+++ Extras/Arquivos/Produtos/menu.py
@@ -7,28 +7,34 @@ class Producto:
     def __str__(self):
         return f"Nome: {self.nome}\nCantidade: {self.cantidade}\nPrezo: {self.prezo}"
 
-lsprods = []
+while True:
+    lsprods = []
 
-with open("produtos.csv", "r") as arquivo:
+    with open("produtos.csv", "r") as arquivo:
         next(arquivo)
         produtos = arquivo.readlines()
         for produto in produtos:
             prod = Producto(produto.split(",")[0], produto.split(",")[1], produto.split(",")[2])
             lsprods.append(prod)
 
-print('''1. Listar productos.
+    print('''1. Listar productos.
 2. Crear productos.
 99. Saír.
-''')
+    ''')
 
-opcion = int(input("Opción: "))
+    opcion = int(input("Opción: "))
 
-match opcion:
-    case 99:
-        exit()
-    case 1:
-        for prod in lsprods:
-            print(prod)
+    match opcion:
+        case 99:
+            exit()
+        case 1:
+            for prod in lsprods:
+                print(prod)
 
-    case _:
-        print("Opción inválida.")
+        case 2:
+            prod = Producto(input("Nome: "), input("Cantidade: "), input("Prezo: "))
+            with open("produtos.csv", "a") as arquivo:
+                arquivo.write(f"\n{prod.nome},{prod.cantidade},{prod.prezo}")
+
+        case _:
+            print("Opción inválida.")
