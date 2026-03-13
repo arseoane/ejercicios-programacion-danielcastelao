1812912b2653a58c718b620e9ef713cef80f4cab
arseoane <arodriguezseoane@danielcastelao.org>
2026-03-13 10:15:35 +0100

    Comezo exercicio 7

---
+++ Boletín 11/exercicio7.py
@@ -0,0 +1,7 @@
+# Descarga da páxina de euroestat os datos en formato csv da táboa que mostra esta url. Facer o programa que permita extraer a seguinte información sobre o consumo de enerxía renovable:
+# A produción total de enerxía renovable por país no período nos que están tomados os datos.
+# A produción de enerxía renovable dun país nun ano especificado.
+# Pais con maior crecemento porcentual dende o primeiro ano da estatística ata o último.
+# Pais con maior produción de enerxía renovable nun ano especificado.
+# O programa ofrecerá un menú coas opcións, os períodos, países e anos se introducirán por teclado é o resultado se mostrará por pantalla.
+
